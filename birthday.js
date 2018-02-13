72f05ca3c7ab80070744f920bf37380662169c82
Abraham  Gonzalez <ag32205@srcs.loc>
2018-02-13 12:18:09 -0800

    days until b day

--- birthday.js
+++ birthday.js
@@ -17,6 +17,13 @@ function displayAge() {
 
 
 //////////
+       function daysUntilBday(birthday) {
+           var bDay = new Date(birthday);
+           var today = new Date();
+           var days = Math.floor((bDay.getTime() - today.getTime()) / 1000 / 60 / 60 / 24);
+           return days
+       }
+
         //////////
 
             var button2 = document.getElementById('button2')
