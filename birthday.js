9353291941efb180e24ba68ae2b055f6e5eb00b5
Nicole I Stein <ns60549@srcs.loc>
2018-02-15 11:44:36 -0800

    Beautify

--- birthday.js
+++ birthday.js
@@ -3,12 +3,13 @@ var button = document.getElementById('button1');
 function displayAge() {
     var input = document.getElementById('textbox');
     var bday = input.getDate();
-            var today = new Date();
-            var time = (today.getTime() - this.birthdate.getTime());
-            var age = Math.floor((today.getTime() - Math.floor(this.birthdate.getTime())) / (1000 * 60 * 60 * 24 * 365));
-            document.getElementById('submit1').textContent = "You are " age " years old."
+    var today = new Date();
+    var time = (today.getTime() - this.birthdate.getTime());
+    var age = Math.floor((today.getTime() - Math.floor(this.birthdate.getTime())) / (1000 * 60 * 60 * 24 * 365));
+    document.getElementById('submit1').textContent = "You are "
+    age " years old."
 }
-            button.addEventListener('click', displayAge);
+button.addEventListener('click', displayAge);
 
 
 
@@ -16,41 +17,42 @@ function displayAge() {
 
 
 
-      function daysUntilBDay(birthday) {
+function daysUntilBDay(birthday) {
     var bDay = new Date(birthday);
     var today = new Date();
     var days = Math.floor((bDay.getTime() - today.getTime()) / 1000 / 60 / 60 / 24 / 24);
     return days;
-      }
+}
 
 
 
-
-            var button2 = document.getElementById('textbox')
-            var month = this.birthdate.getMonth() + 1;
-            var day = this.birthdate.getDate() + 1;
-            if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
-                return "Your sign is Capricorn";
-            } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
-                return "Your sign is Aquarius";
-            } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
-                return "Your sign is Pisces";
-            } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
-                return "Your sign is Aries";
-            } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
-                return "Your sign is Taurus";
-            } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
-                return "Your sign is Gemini";
-            } else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
-                return "Your sign is Cancer";
-            } else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
-                return "Your sign is Leo";
-            } else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
-                return "Your sign is Virgo";
-            } else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
-                return "Your sign is Libra";
-            } else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
-                return "Your sign is Scorpio";
-            } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
-                return "Your sign is Sagittarius";
-            }
+function displaySign() {
+    var button2 = document.getElementById('textbox')
+    var month = this.birthdate.getMonth() + 1;
+    var day = this.birthdate.getDate() + 1;
+    if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
+        return "Your sign is Capricorn";
+    } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+        return "Your sign is Aquarius";
+    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        return "Your sign is Pisces";
+    } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+        return "Your sign is Aries";
+    } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+        return "Your sign is Taurus";
+    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        return "Your sign is Gemini";
+    } else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+        return "Your sign is Cancer";
+    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+        return "Your sign is Leo";
+    } else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+        return "Your sign is Virgo";
+    } else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+        return "Your sign is Libra";
+    } else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+        return "Your sign is Scorpio";
+    } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+        return "Your sign is Sagittarius";
+    }
+}
