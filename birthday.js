4171cf8b86669bdaf78c2ef7b879c1379ec89a17
Kyle  Abey <ka65821@srcs.loc>
2018-02-28 11:42:32 -0800

    function displayAge now works

--- birthday.js
+++ birthday.js
@@ -1,14 +1,16 @@
-var button1 = document.getElementById('giveAge');
-button1.addEventListener('click', () => {
-    document.getElementById("submit1").textContent = displayAge()
+var button1 = document.getElementById("giveAge");
+button1.addEventListener("click", () => {
+    document.getElementById("output").textContent = displayAge();
+
 });
 
+
 function displayAge() {
-    var input = document.getElementById('textbox').value;
-    var bday = input.getDate();
+    var input = document.getElementById("textbox").value;
+    var bday = new Date(input);
     var today = new Date();
     var age = Math.floor((today.getTime() - Math.floor(bday.getTime())) / (1000 * 60 * 60 * 24 * 365));
-    document.getElementById('submit1').textContent = "You are " + age + " years old.";
+    return "You are " + age + " years old.";
 }
 
 
